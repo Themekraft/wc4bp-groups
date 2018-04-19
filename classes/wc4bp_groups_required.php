5b32000084c27ea0cd8b4851311412d6b8840b75
Guillermo <gfirem@gmail.com>
2018-04-19 09:18:35 -0300

    * Adding custom message to check dependency. * Changing the requirement library to use a custom internal id.

--- classes/wc4bp_groups_required.php
+++ classes/wc4bp_groups_required.php
@@ -24,7 +24,7 @@ class wc4bp_groups_required {
 		if ( ! is_admin() ) {
 			return;
 		}
-		add_action( 'tgmpa_register', array( $this, 'setup_and_check' ) );
+		add_action( 'wc4bp_group_tgmpa_register', array( $this, 'setup_and_check' ) );
 		add_action( 'in_admin_footer', array( $this, 'remove_woo_footer' ) );
 	}
 
@@ -135,26 +135,26 @@ class wc4bp_groups_required {
 			'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
 			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-			'strings'      => array(
-				'notice_can_install_required'    => _n_noop(
+			'strings' => array(
+				'notice_can_install_required'     => _n_noop(
 				/* translators: 1: plugin name(s). */
-					'One of the <u>ThemKraft</u> plugin requires the following plugin: %1$s.',
-					'One of the <u>ThemKraft</u> plugin requires the following plugins: %1$s.',
+					'<u>WC4BP -> Groups</u> plugin requires the following plugin: %1$s.',
+					'<u>WC4BP -> Groups</u> plugin requires the following plugins: %1$s.',
 					'wc4bp_groups'
 				),
-				'notice_can_install_recommended' => _n_noop(
+				'notice_can_install_recommended'  => _n_noop(
 				/* translators: 1: plugin name(s). */
-					'One of the <u>ThemKraft</u> plugin recommends the following plugin: %1$s.',
-					'One of the <u>ThemKraft</u> plugin recommends the following plugins: %1$s.',
+					'<u>WC4BP -> Groups</u> plugin recommends the following plugin: %1$s.',
+					'<u>WC4BP -> Groups</u> plugin recommends the following plugins: %1$s.',
 					'wc4bp_groups'
 				),
-				'notice_can_activate_required'   => _n_noop(
+				'notice_can_activate_required'    => _n_noop(
 				/* translators: 1: plugin name(s). */
-					'The following is a required plugin for one of the <u>ThemKraft</u> and is currently inactive: %1$s.',
-					'The following is a required plugins for one of the <u>ThemKraft</u> and they are currently inactive: %1$s.',
+					'The following is a required plugin for <u>WC4BP -> Groups</u> and is currently inactive: %1$s.',
+					'The following is a required plugins for <u>WC4BP -> Groups</u> and they are currently inactive: %1$s.',
 					'wc4bp_groups'
 				),
-				'notice_ask_to_update'           => _n_noop(
+				'notice_ask_to_update'            => _n_noop(
 				/* translators: 1: plugin name(s). */
 					'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this plugin: %1$s.',
 					'The following plugins need to be updated to their latest version to ensure maximum compatibility with this plugin: %1$s.',
@@ -164,7 +164,7 @@ class wc4bp_groups_required {
 		);
 
 		// Call the tgmpa function to register the required required_plugins
-		tgmpa( $required_plugins, $config );
+		wc4bp_group_tgmpa( $required_plugins, $config );
 	}
 
 }
